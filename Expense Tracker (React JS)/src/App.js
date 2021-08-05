c025c4f2c8584e34752398a557a7f1f76c5b5fa9
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-08-05 10:30:19 +0530

    added expense row component

--- Expense Tracker (React JS)/src/App.js
+++ Expense Tracker (React JS)/src/App.js
@@ -1,7 +1,12 @@
+// 1. importing ExpenseItem.js component
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      {/* 2. using ExpenseItem.js component as JSX tags */}
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ Expense Tracker (React JS)/src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+// 1. created our first component (it's just a function which returns a HTML)
+// 2. It is not really HTML, it is JSX which is dev by React team
+function ExpenseItem() {
+   // 3. setting fake dynamic data to show using JSX
+   const expenseDate = new Date(2021, 2, 28);
+   const expenseTitle = "Car Insurance";
+   const expenseAmount = 278;
+
+   // 4. adding these const in the JXS tags and returning
+   return (
+      <div className="expense-item">
+         <div>{expenseDate.toISOString()}</div>
+         <div className="expense-item__description">
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">Rs.{expenseAmount}</div>
+         </div>
+      </div>
+   );
+}
+
+// 5. exporting the component & importing in app.js
+export default ExpenseItem;
