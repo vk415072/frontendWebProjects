72445827e83095b48e99e9c50631f80bcd8037bf
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-08-05 15:34:58 +0530

    expense item component update
    Sending data to component from outside to make it reusable.

--- Expense Tracker (React JS)/src/App.js
+++ Expense Tracker (React JS)/src/App.js
@@ -2,13 +2,45 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      {/* 2. using ExpenseItem.js component as JSX tags */}
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
+   // 3. creating data for ExpenseItem
+   const expenses = [
+      {
+         id: "e1",
+         title: "Car Wash",
+         amount: "65",
+         date: new Date(2021, 6, 12),
+      },
+      {
+         id: "e2",
+         title: "Kids Shopping",
+         amount: "205",
+         date: new Date(2021, 6, 9),
+      },
+      {
+         id: "e3",
+         title: "Ice Cream",
+         amount: "25",
+         date: new Date(2021, 6, 12),
+      },
+      {
+         id: "e4",
+         title: "Shopping",
+         amount: "89",
+         date: new Date(2021, 5, 10),
+      },
+   ];
+
+   return (
+      <div>
+         <h2>Let's get started!</h2>
+         {/* 2. using ExpenseItem.js component as JSX tags */}
+         {/* 4. creating the attributes with data expenses data created above */}
+         <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+         <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+         <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+         <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      </div>
+   );
 }
 
 export default App;
--- Expense Tracker (React JS)/src/components/ExpenseItem.js
+++ Expense Tracker (React JS)/src/components/ExpenseItem.js
@@ -3,19 +3,22 @@ import "./ExpenseItem.css";
 
 // 1. created our first component (it's just a function which returns a HTML)
 // 2. It is not really HTML, it is JSX which is dev by React team
-function ExpenseItem() {
+// 3. getting property from app.js set attributes. This will make this component reusable.
+function ExpenseItem(props) {
    // 3. setting fake dynamic data to show using JSX
-   const expenseDate = new Date(2021, 2, 28);
-   const expenseTitle = "Car Insurance";
-   const expenseAmount = 278;
+   // 4. sending this data into app.js file
+   // const expenseDate = new Date(2021, 2, 28);
+   // const expenseTitle = "Car Insurance";
+   // const expenseAmount = 278;
 
    // 4. adding these const in the JXS tags and returning
    return (
+      // 7. now accessing data with properties from app.js
       <div className="expense-item">
-         <div>{expenseDate.toISOString()}</div>
+         <div>{props.date.toISOString()}</div>
          <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">Rs.{expenseAmount}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">Rs.{props.amount}</div>
          </div>
       </div>
    );
